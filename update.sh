4fdb01e1b24be313c7384cb37e29b665908ea5d2
Nikilauz <myinternetid@gmx.de>
2021-02-05 15:48:04 +0100

    Guarantee existance of db files

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mv cookbook.json _cookbook.json
-mv accounts.json _accounts.json
+cp cookbook.json _cookbook.json
+cp accounts.json _accounts.json
 git pull
 mv -f _cookbook.json cookbook.json
 mv -f _accounts.json accounts.json
